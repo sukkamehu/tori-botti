687c6e36075a7d6decb9b66f5e0db05ce26a2218
Juuso Saari <juusos93@gmail.com>
2021-04-27 23:52:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ let transporter = nodemailer.createTransport(settings.email_service);
 let autoChecker = {
   itemsAlerted: [], // keep list to not send report of matches twice
   makeCheck: function(){
-    setInterval(function(){
+    setInterval(() => {
       let url = `https://www.tori.fi/${settings.region}?q=${settings.searching_for}`;
       console.log(new Date(), "Checking for new items, url: ",url)
-      axios.get(url).then(function(response){
+      axios.get(url).then((response) => {
         const $ = cheerio.load(response.data, { decodeEntities: true });
         let itemsToAlert = [];
         let emailHtml = "";
-        $('#blocket > div.main > div > div > div.list_mode_thumb a').each(function(i, elm) {
+        $('#blocket > div.main > div > div > div.list_mode_thumb a').each((i, elm) => {
           if(elm.attribs.id){
             if(!autoChecker.itemsAlerted.includes(elm.attribs.id)){
               itemsToAlert.push(elm.attribs.id)
@@ -37,23 +37,18 @@ let autoChecker = {
     }, settings.check_interval_mins * 60 * 1000)
   },
   sendMail: function(emailHtml, itemsToAlert){
-
-
-
-    let subject = `Löytyi ${itemsToAlert.length} uutta artikkelia haulla ${settings.searching_for}! 🍀🍀🍀`
-    let from = `"ToriBotti 🤖" <toribotti@tori.fi>`
+    let from = `"ToriBotti 👻" <toribotti@tori.fi>`
     if(settings.email_service.auth.user){
-      from = `"ToriBotti 🤖" <${settings.email_service.auth.user}>`
+      from = `"ToriBotti 👻" <${settings.email_service.auth.user}>`; // just to prevent possible mail server errors
     }
 
     transporter.sendMail({
       from: from,
       to: settings.report_emails,
-      subject: subject,
-      html: `<html><head><meta charset="UTF-8"></head><body>${emailHtml}<br /><h2>Toivottavasti löytyi!</h2></body></html>`
-    }).catch(function(e){console.log(new Date(), "error sending mail",e)});
+      subject: `Tori🤖 löysi ${itemsToAlert.length} uutta artikkelia haulla ${settings.searching_for}!`,
+      html: `<html><head><meta charset="UTF-8"></head><body>${emailHtml}</body></html>`
+    }).then((console.log("Message sent!"))).catch((e) => {console.log(new Date(), "error sending mail",e)});
 
-    console.log("Message sent!");
     autoChecker.itemsAlerted = autoChecker.itemsAlerted.concat(itemsToAlert)
   }
 }
